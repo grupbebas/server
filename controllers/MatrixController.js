950776c142e671e641022f08e86aa8af191bd7a0
agungatd <agungatidhira@gmail.com>
2018-11-02 10:47:01 +0700

    finish basic

--- controllers/MatrixController.js
+++ controllers/MatrixController.js
@@ -9,11 +9,11 @@ class MatrixController {
     let address = req.body.address
 
     axios({
-      url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${latOrigin},${lngOrigin}&destinations=${address}&key=${process.env.GOOGLE_PLACE_API_KEY}`,
+      url: `https://maps.googleapis.com/maps/api/directions/json?units=metric&origin=${latOrigin},${lngOrigin}&destination=${address}&key=${process.env.GOOGLE_PLACE_API_KEY}`,
       method: 'get'
     })
       .then(response => {
-        let data = response.data.rows
+        let data = response.data
         console.log('data::', response.data)
         res.status(200).json({
           status: 'success',
