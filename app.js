f03cdfbb22e51b729b8af1dad5085d8f67cc6a7d
dwiwicaksono90 <dwiw1909@gmail.com>
2018-11-02 10:37:42 +0700

    config user login register and zomato

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/groupw1', {useNewUrlParser: true});
 
 var usersRouter = require('./routes/users');
+var zomatoRouter = require('./routes/zomato')
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/users', usersRouter);
+app.use('/zomato', zomatoRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
